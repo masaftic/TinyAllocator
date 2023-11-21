599ece9df31bbeb2f4b5d4ff2f0eeabddfba0452
masaftic <yousefalaa618y@gmail.com>
2023-11-21 15:52:50 +0200

    ready set go..

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define CAPACITY 64000
+
+char heap[CAPACITY] = {0};
+
+/// @brief allocate {size} of bytes in the heap
+/// @param size 
+/// @return return ptr to allocated memory 
+void *heap_alloc(size_t size)
+{
+
+}
+
+/// @brief frees memory pointed to by ptr
+/// @param ptr 
+void heap_free(void *ptr)
+{
+
+}
